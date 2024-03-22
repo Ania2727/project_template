fb8d6c551b0165a5a660be614c8a0a91d842105e
Anna Kuznetsova <vomi.anya@gmail.com>
2024-03-22 15:29:48 +0200

    Added functions

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+
+def input_text_console():
+    pass
+
+
+def read_text_file():
+    pass
+
+
+def read_with_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,11 @@
+
+def output_text_console():
+    pass
+
+
+def write_to_file():
+    pass
+
+
+def write_with_pandas():
+    pass
