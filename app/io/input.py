b3c477d0df8e6de114fd946a7f7b734338dd9ef1
Anna Kuznetsova <vomi.anya@gmail.com>
2024-03-22 16:19:46 +0200

    Added docstrings

--- app/io/input.py
+++ app/io/input.py
@@ -1,11 +1,36 @@
 
 def input_text_console():
-    pass
+    """
+        Reads text input from the console.
+
+        Returns:
+            str: The text entered by the user.
+        """
+    user_input = input("Enter your text")
+    return user_input
 
 
 def read_text_file():
+    """
+        Reads the contents of a text file using built-in Python functions.
+
+        Args:
+            data.txt (str): Path to the file from which data should be read.
+
+        Returns:
+            str: The content of the file as a string.
+        """
     pass
 
 
 def read_with_pandas():
+    """
+        Reads the contents of a text file using the pandas library.
+
+        Args:
+            data.txt (str): Path to the file from which data should be read.
+
+        Returns:
+            pandas.DataFrame: Data from the file as a DataFrame.
+        """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,11 +1,31 @@
 
 def output_text_console():
+    """
+        Outputs text to the console.
+
+        Args:
+            "some text" (str): The text to be displayed.
+        """
     pass
 
 
 def write_to_file():
+    """
+        Writes text to a file using built-in Python functions.
+
+        Args:
+            data.txt (str): Path to the file where data should be written.
+            "some text" (str): The text to be written to the file.
+        """
     pass
 
 
 def write_with_pandas():
+    """
+        Writes data from a DataFrame to a file using the pandas library.
+
+        Args:
+            data.txt (str): Path to the file where data should be written.
+            dataframe. (pandas.DataFrame): The data to be written to the file.
+        """
     pass
